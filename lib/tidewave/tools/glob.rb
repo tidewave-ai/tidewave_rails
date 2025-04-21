9729b767ebdd1ac7b4552a00a0eff2cd3d65d8fa
Yorick Jacquin <yorickjacquin@gmail.com>
2025-04-21 15:32:56 +0200

    Add glob tool

---
+++ lib/tidewave/tools/glob.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "tidewave/file_tracker"
+
+class Tidewave::Tools::Glob < Tidewave::Tools::Base
+  tool_name "glob"
+  description "Searches for files matching the given glob pattern."
+
+  arguments do
+    required(:pattern).filled(:string).description('The glob pattern to match files against, e.g., \"**/*.ex\"')
+  end
+
+  def call(pattern:)
+    Dir.glob(pattern, base: Tidewave::FileTracker.git_root)
+  end
+end
