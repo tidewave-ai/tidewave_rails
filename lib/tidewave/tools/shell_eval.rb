2cb8689565cf075640e876a49462fa04080138bb
José Valim <jose.valim@dashbit.co>
2025-04-30 16:14:44 +0200

    Improve shell eval description
    Otherwise the tool may decide to invoke irb or similar.

--- lib/tidewave/tools/shell_eval.rb
+++ lib/tidewave/tools/shell_eval.rb
@@ -10,10 +10,12 @@ class Tidewave::Tools::ShellEval < Tidewave::Tools::Base
   description <<~DESCRIPTION
     Executes a shell command in the project root directory.
 
-    Avoid using this tool for file operations. Instead, rely on dedicated file system tools, if available.
-
     The operating system is of flavor #{RUBY_PLATFORM}.
 
+    Avoid using this tool for file operations. Instead, rely on
+    dedicated file system tools, if available.
+
+    Do not use this tool to evaluate Ruby code. Use `project_eval` instead.        
     Only use this tool if other means are not available.
   DESCRIPTION
 
