6e9d49ea5fd4e1c5c06dbb732f64ca7028e3b5e2
Radosław Woźniak <184065+radwo@users.noreply.github.com>
2025-05-01 16:16:49 +0200

    feat: allow configuratin from rails

---
+++ lib/tidewave/configuration.rb
@@ -0,0 +1,12 @@
+module Tidewave
+  class Configuration
+    attr_accessor :logger, :allowed_origins, :localhost_only, :allowed_ips
+
+    def initialize
+      @logger = Logger.new(STDOUT)
+      @allowed_origins = nil
+      @localhost_only = nil
+      @allowed_ips = nil
+    end
+  end
+end
--- lib/tidewave/railtie.rb
+++ lib/tidewave/railtie.rb
@@ -4,12 +4,17 @@ require "fast_mcp"
 require "logger"
 require "fileutils"
 require "tidewave/tool_resolver"
+require "tidewave/configuration"
 
 module Tidewave
   class Railtie < Rails::Railtie
+    config.tidewave = Tidewave::Configuration.new
+
     initializer "tidewave.setup_mcp" do |app|
       # Prevent MCP server from being mounted if Rails is not running in development mode
       raise "For security reasons, Tidewave is only supported in development mode" unless Rails.env.development?
+
+      config = app.config.tidewave
 
       # Set up MCP server with the host application
       FastMcp.mount_in_rails(
@@ -19,7 +24,10 @@ module Tidewave
         path_prefix: Tidewave::PATH_PREFIX,
         messages_route: Tidewave::MESSAGES_ROUTE,
         sse_route: Tidewave::SSE_ROUTE,
-        logger: Logger.new(STDOUT)
+        logger: config.logger,
+        allowed_origins: config.allowed_origins,
+        localhost_only: config.localhost_only,
+        allowed_ips: config.allowed_ips
       ) do |server|
         app.config.before_initialize do
           # Register a custom middleware to register tools depending on `include_fs_tools` query parameter
