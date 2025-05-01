5a1b7486458e2273e93ecc68d467af0b744c5429
Yorick Jacquin <yorickjacquin@gmail.com>
2025-05-01 11:50:55 +0200

    Implement descendants over rails monkey patch

--- lib/tidewave/tools/base.rb
+++ lib/tidewave/tools/base.rb
@@ -3,6 +3,19 @@
 module Tidewave
   module Tools
     class Base < FastMcp::Tool
+      @descendants = []
+
+      def self.inherited(subclass)
+        @descendants ||= []
+        @descendants << subclass
+
+        super
+      end
+
+      def self.descendants
+        @descendants || []
+      end
+
       def self.file_system_tool
         @file_system_tool = true
       end
