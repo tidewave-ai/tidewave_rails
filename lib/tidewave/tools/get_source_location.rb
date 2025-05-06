187b0aba27b3c727d7ed2defbbc08a1f236ca78d
José Valim <jose.valim@dashbit.co>
2025-05-06 20:23:30 +0200

    Use reference in get_source_location instead of Elixir-ish names

--- lib/tidewave/tools/get_source_location.rb
+++ lib/tidewave/tools/get_source_location.rb
@@ -7,52 +7,56 @@ class Tidewave::Tools::GetSourceLocation < Tidewave::Tools::Base
   tool_name "get_source_location"
 
   description <<~DESCRIPTION
-    Returns the source location for the given module (or function).
+    Returns the source location for the given reference.
 
-    This works for modules in the current project, as well as dependencies.
+    It may be a class/module, such as `String`, an instance method,
+    such as `String#gsub`, or class method, such as `File.executable?`
 
-    This tool only works if you know the specific module (and optionally function) that is being targeted.
+    This works for methods in the current project, as well as dependencies.
+
+    This tool only works if you know the specific method that is being targeted.
     If that is the case, prefer this tool over grepping the file system.
   DESCRIPTION
 
   arguments do
-    required(:module_name).filled(:string).description("The module to get source location for. When this is the single argument passed, the entire module source is returned.")
-    optional(:function_name).filled(:string).description("The function to get source location for. When used, a module must also be passed.")
+    required(:reference).filled(:string).description("The class/module/method to lookup, such String, String#gsub or File.executable?")
   end
 
+  def call(reference:)
+    file_path, line_number = get_source_location(reference)
 
-  def call(module_name:, function_name: nil)
-    file_path, line_number = get_source_location(module_name, function_name)
-
+    if file_path
     {
       file_path: file_path,
       line_number: line_number
     }.to_json
+    else
+      raise NameError, "Could not find source location for #{reference}"
+    end
   end
 
   private
 
-  def get_source_location(module_name, function_name)
-    begin
-      module_ref = module_name.constantize
-    rescue NameError
-      raise NameError, "Module #{module_name} not found"
+  def get_source_location(reference)
+    constant_name, selector, method_name = reference.rpartition(/\.|#/)
+
+    # This is a class/module lookup
+    return Object.const_source_location(method_name) if selector.empty?
+
+    constant = constant_name.constantize
+
+    if selector == "#"
+      begin
+        constant.instance_method(method_name).source_location
+      rescue
+        raise NameError, "Could not find instance method #{method_name} on #{constant_name}"
+      end
+    else
+      begin
+        constant.method(method_name).source_location
+      rescue
+        raise NameError, "Could not find class method #{method_name} on #{constant_name}"
+      end
     end
-
-    return get_method_definition(module_ref, function_name) if function_name.present?
-
-    module_ref.const_source_location(module_name)
-  end
-
-  def get_method_definition(module_ref, function_name)
-    module_ref.method(function_name).source_location
-  rescue NameError
-    get_instance_method_definition(module_ref, function_name)
-  end
-
-  def get_instance_method_definition(module_ref, function_name)
-    module_ref.instance_method(function_name).source_location
-  rescue NameError
-    raise NameError, "Method #{function_name} not found in module #{module_ref.name}"
   end
 end
--- spec/tools/get_source_location_spec.rb
+++ spec/tools/get_source_location_spec.rb
@@ -16,17 +16,12 @@ describe Tidewave::Tools::GetSourceLocation do
   describe ".description" do
     let(:description) do
       <<~DESCRIPTION
-        Returns the source location for the given module (or function).
-
-        This works for modules in the current project, as well as dependencies.
-
-        This tool only works if you know the specific module (and optionally function) that is being targeted.
-        If that is the case, prefer this tool over grepping the file system.
+        Returns the source location for the given reference.
       DESCRIPTION
     end
 
     it "returns the correct description" do
-      expect(described_class.description).to eq(description)
+      expect(described_class.description).to match(description)
     end
   end
 
@@ -34,16 +29,12 @@ describe Tidewave::Tools::GetSourceLocation do
     let(:expected_input_schema) do
       {
         properties: {
-          module_name: {
+          reference: {
             type: "string",
-            description: "The module to get source location for. When this is the single argument passed, the entire module source is returned."
+            description: "The class/module/method to lookup, such String, String#gsub or File.executable?"
           },
-          function_name: {
-            type: "string",
-            description: "The function to get source location for. When used, a module must also be passed."
-          }
         },
-        required: [ "module_name" ],
+        required: [ "reference" ],
         type: "object"
       }
     end
@@ -54,79 +45,96 @@ describe Tidewave::Tools::GetSourceLocation do
   end
 
   describe "#call" do
-    context 'with module_name as argument' do
-      subject { described_class.new.call_with_schema_validation!(module_name: module_name) }
+    subject { described_class.new.call_with_schema_validation!(reference: reference) }
 
-      let(:module_name) { 'TidewaveTestModule' }
-      let(:foo_line_number) { __LINE__ + 3 }
-      let(:bar_line_number) { __LINE__ + 6 }
-      test_module = Class.new do
-        def self.foo
-          'foo'
-        end
-
-        def bar
-          'bar'
-        end
+    let(:foo_line_number) { __LINE__ + 3 }
+    let(:bar_line_number) { __LINE__ + 6 }
+    test_module = Class.new do
+      def self.foo
+        'foo'
       end
 
-      # This is a bit of a hack to get the line number of the module definition.
-      # It's not the best way to do it, but it's the only way I can think of to get the line number of the module definition.
-      # If you know a better way, please let me know.
-      let(:line_number) { __LINE__ + 1 }
-      Object.const_set(:TidewaveTestModule, test_module)
-
-      context "when the module is found" do
-        it "returns the correct result" do
-          expect(subject).to eq({
-            file_path: __FILE__,
-            line_number: line_number
-          }.to_json)
-        end
+      def bar
+        'bar'
       end
+    end
 
-      context "when the module is not found" do
-        let(:module_name) { "NonExistentModule" }
+    # This is a bit of a hack to get the line number of the module definition.
+    # It's not the best way to do it, but it's the only way I can think of to get the line number of the module definition.
+    # If you know a better way, please let me know.
+    let(:line_number) { __LINE__ + 1 }
+    Object.const_set(:TidewaveTestModule, test_module)
 
-        it "returns nil" do
-          expect { subject }.to raise_error(NameError, "Module NonExistentModule not found")
-        end
+    context "when the module is found" do
+      let(:reference) { 'TidewaveTestModule' }
+
+      it "returns the correct result" do
+        expect(subject).to eq({
+          file_path: __FILE__,
+          line_number: line_number
+        }.to_json)
       end
+    end
 
-      context 'with function_name as argument' do
-        subject { described_class.new.call(module_name: module_name, function_name: function_name) }
+    context "when the module is not found" do
+      let(:reference) { "NonExistentModule" }
 
-        let(:class_method_name) { 'foo' }
-        let(:instance_method_name) { 'bar' }
-        let(:non_existent_method_name) { 'baz' }
+      it "raises" do
+        expect { subject }.to raise_error(NameError, "Could not find source location for NonExistentModule")
+      end
+    end
 
-        context "when the class method exists" do
-          let(:function_name) { class_method_name }
-          it "returns the correct result" do
-            expect(subject).to eq({
-              file_path: __FILE__,
-              line_number: foo_line_number
-            }.to_json)
-          end
-        end
+    context "when the module is invalid" do
+      let(:reference) { "1+2" }
 
-        context "when the instance method exists" do
-          let(:function_name) { instance_method_name }
-          it "returns the correct result" do
-            expect(subject).to eq({
-              file_path: __FILE__,
-              line_number: bar_line_number
-            }.to_json)
-          end
-        end
+      it "raises" do
+        expect { subject }.to raise_error(NameError, "wrong constant name 1+2")
+      end
+    end
 
-        context "when the method does not exist" do
-          let(:function_name) { non_existent_method_name }
+    context "when the module is invalid but the method is valid" do
+      let(:reference) { "1+2#gsub" }
 
-          it "returns nil" do
-            expect { subject }.to raise_error(NameError, "Method baz not found in module TidewaveTestModule")
-          end
-        end
+      it "raises" do
+        expect { subject }.to raise_error(NameError, "wrong constant name 1+2")
+      end
+    end
+
+    context "when the class method exists" do
+      let(:reference) { 'TidewaveTestModule.foo' }
+
+      it "returns the correct result" do
+        expect(subject).to eq({
+          file_path: __FILE__,
+          line_number: foo_line_number
+        }.to_json)
+      end
+    end
+
+    context "when the class method does not exist" do
+      let(:reference) { 'TidewaveTestModule.unknown' }
+
+      it "raises" do
+        expect { subject }.to raise_error(NameError, "Could not find class method unknown on TidewaveTestModule")
+      end
+    end
+
+    context "when the instance method exists" do
+      let(:reference) { 'TidewaveTestModule#bar' }
+
+      it "returns the correct result" do
+        expect(subject).to eq({
+          file_path: __FILE__,
+          line_number: bar_line_number
+        }.to_json)
+      end
+    end
+
+    context "when the instance method does not exist" do
+      let(:reference) { 'TidewaveTestModule#unknown' }
+
+      it "raises" do
+        expect { subject }.to raise_error(NameError, "Could not find instance method unknown on TidewaveTestModule")
       end
     end
   end
