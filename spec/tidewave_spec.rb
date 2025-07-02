af0dc307c2665918932886149b4e2bd0d814dbd4
José Valim <jose.valim@dashbit.co>
2025-07-02 11:12:49 +0200

    Also test injected Rails config

--- spec/tidewave_spec.rb
+++ spec/tidewave_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Tidewave do
   it "has a version number" do
     expect(Tidewave::VERSION).not_to be nil
   end
+
+  it "injects Rails configuration" do
+    config = Rails.configuration.tidewave
+    expect(config).to be_a(Tidewave::Configuration)
+    expect(config.logger.instance_variable_get(:@logdev).filename).to eq(Rails.root.join("log/tidewave.log"))
+  end
 end
