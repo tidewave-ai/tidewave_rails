62aef3b0a582008f25c705c4d4bdd72938a44c1f
Filipe Mendes da Rocha Lopes <filipemendespi@gmail.com>
2025-07-14 19:50:46 -0300

    Update lib/tidewave/database_adapter.rb
    Co-authored-by: José Valim <jose.valim@gmail.com>

--- lib/tidewave/database_adapter.rb
+++ lib/tidewave/database_adapter.rb
@@ -8,7 +8,7 @@ module Tidewave
       end
 
       def create_adapter
-        orm_type = detect_orm
+        orm_type = Rails.application.config.tidewave.preferred_orm
         case orm_type
         when :active_record
           require_relative "database_adapters/active_record"
@@ -17,50 +17,7 @@ module Tidewave
           require_relative "database_adapters/sequel"
           DatabaseAdapters::Sequel.new
         else
-          raise "No supported ORM detected. Please ensure ActiveRecord or Sequel is available."
-        end
-      end
-
-      private
-
-      def detect_orm
-        # Check for preferred ORM setting first
-        if Rails.application.config.respond_to?(:tidewave)
-          preferred = Rails.application.config.tidewave.preferred_orm
-          if preferred && [ :active_record, :sequel ].include?(preferred)
-            return preferred if orm_available?(preferred)
-          end
-        end
-
-        # Auto-detect based on what's defined
-        if defined?(::ActiveRecord::Base)
-          :active_record
-        elsif defined?(::Sequel::Model)
-          :sequel
-        else
-          # Try to require them to see if they're available
-          begin
-            require "active_record"
-            :active_record
-          rescue LoadError
-            begin
-              require "sequel"
-              :sequel
-            rescue LoadError
-              nil
-            end
-          end
-        end
-      end
-
-      def orm_available?(orm)
-        case orm
-        when :active_record
-          !!defined?(::ActiveRecord::Base) || (require("active_record") rescue false)
-        when :sequel
-          !!defined?(::Sequel::Model) || (require("sequel") rescue false)
-        else
-          false
+          raise "Unknown preferred ORM: #{orm_type}"
         end
       end
     end
