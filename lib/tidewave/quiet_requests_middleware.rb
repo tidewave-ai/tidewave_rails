f7ad58d3a1e8352a66e972c6e47a6c3b5194aaf3
Jonatan Kłosko <jonatanklosko@gmail.com>
2025-08-13 10:05:19 +0200

    Update lib/tidewave/quiet_requests_middleware.rb
    Co-authored-by: José Valim <jose.valim@dashbit.co>

--- lib/tidewave/quiet_requests_middleware.rb
+++ lib/tidewave/quiet_requests_middleware.rb
@@ -6,7 +6,7 @@ class Tidewave::QuietRequestsMiddleware < Rails::Rack::Logger
   end
 
   def call(env)
-    if env["PATH_INFO"].include?("/tidewave")
+    if env["PATH_INFO"].start_with?("/tidewave")
       Rails.logger.silence { super(env) }
     else
       super(env)
