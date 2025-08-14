d0ba43b1e0b8137064d1d05a9598eb930c126cb0
Jonatan Kłosko <jonatanklosko@gmail.com>
2025-08-14 19:32:59 +0200

    Do not log tidewave requests (#76)
    Co-authored-by: José Valim <jose.valim@dashbit.co>

---
+++ lib/tidewave/quiet_requests_middleware.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Tidewave::QuietRequestsMiddleware
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    if env["PATH_INFO"].start_with?("/tidewave")
+      Rails.logger.silence { @app.call(env) }
+    else
+      @app.call(env)
+    end
+  end
+end
