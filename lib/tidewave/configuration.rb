a8c3627f6c137c82cb150496f7acbd5647f592d2
Jonatan Kłosko <jonatanklosko@gmail.com>
2025-09-12 20:00:10 +0200

    Remove credentials config (#84)

--- lib/tidewave/configuration.rb
+++ lib/tidewave/configuration.rb
@@ -2,13 +2,13 @@
 
 module Tidewave
   class Configuration
-    attr_accessor :logger, :allow_remote_access, :preferred_orm, :credentials, :client_url, :team
+    attr_accessor :logger, :allow_remote_access, :preferred_orm, :dev, :client_url, :team
 
     def initialize
       @logger = nil
       @allow_remote_access = true
       @preferred_orm = :active_record
-      @credentials = {}
+      @dev = false
       @client_url = "https://tidewave.ai"
       @team = {}
     end
